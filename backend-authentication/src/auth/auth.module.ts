6e683789c629c0ee4d9a59def2925fea4a5dba7a
Andre Eppinghaus <andreeppinghaus@gmail.com>
2025-04-22 15:16:57 -0300

    feat: adicinionando o bcrypt ao modulo

--- backend-authentication/src/auth/auth.module.ts
+++ backend-authentication/src/auth/auth.module.ts
@@ -1,10 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { BcryptService } from './hashing/bcrypt.service';
 
 @Global() //pode ser usado sem importar
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [
+    AuthService,
+    BcryptService
+  ],
 })
-export class AuthModule {}
+export class AuthModule { }
