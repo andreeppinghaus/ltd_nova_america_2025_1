831b2a678e19d5d98c7b94d952a83e24f780edef
Andre Eppinghaus <andreeppinghaus@gmail.com>
2025-04-22 15:26:41 -0300

    feat: criando o metodo hash

--- backend-authentication/src/auth/hashing/bcrypt.service.ts
+++ backend-authentication/src/auth/hashing/bcrypt.service.ts
@@ -1,8 +1,11 @@
 import { HashingService } from "./hashing.service";
 
+import * as bcrypt from 'bcrypt';
+
 export class BcryptService extends HashingService {
     async hash(password: string): Promise<string> {
-        return 'hash senha';
+        const salt = await bcrypt.genSalt();
+        return bcrypt.hash(password, salt);
     }
 
     async compare(password: string, passwordHash: string): Promise<boolean> {
