b3476ee055bde5f9053f77eb9b23d2ee31671763
Andre Eppinghaus <andreeppinghaus@gmail.com>
2025-04-22 15:14:44 -0300

    feat: alterando titulo

--- backend-authentication/src/auth/hashing/hashing.service.ts
+++ backend-authentication/src/auth/hashing/hashing.service.ts
@@ -1,13 +1,13 @@
 /**
  * Servico abstrato de Hash
- * Gera um hash para a senha 
+ * Trata servicos de criptografia
  */
 export abstract class HashingService {
     /**
      * Gera um hash para a senha do usuário
      * @param {string} password senha do usuário
      */
-    abstract hash(password: string): Promise<string>
+    abstract hash(password: string): Promise<string>;
 
     /**
      * Compara se uma senha é condiz com o hash da senha
@@ -17,6 +17,6 @@ export abstract class HashingService {
      * @param {string}passwordHash hash da senha
      * @returns verdadeiro ou falso
      */
-    abstract compare(password: string, passwordHash: string): Promise<boolean>
+    abstract compare(password: string, passwordHash: string): Promise<boolean>;
 
 }
